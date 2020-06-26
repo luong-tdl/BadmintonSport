b5b152cbfe2c4cc9c67f0492946809edd8d7d61f
huynhtri0202 <karintri1999@gmail.com>
2020-06-26 18:48:47 +0700

    AddController

---
+++ Tri/GraniteHouse/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime BirthData { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        [Display(Name= "Vị trí tuyển dụng")]
+        public int VitriEmployeeId { get; set; }
+        [ForeignKey("VitriEmployeeId")]
+        public virtual PositionEmployee PositionEmployee { get; set; }
+    }
+}
