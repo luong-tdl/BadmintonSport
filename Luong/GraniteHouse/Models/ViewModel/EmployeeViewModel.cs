fe36f0e6d4e223136efa2f22e8233b76f5248fff
luong-tdl <66153081+luong-tdl@users.noreply.github.com>
2020-06-26 18:22:09 +0700

    Models

---
+++ Luong/GraniteHouse/Models/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public Employee Employee { get; set; }
+        public IEnumerable<PositionEmployee> PositionEmployees { get; set; }
+    }
+}
